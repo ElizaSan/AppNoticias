753157f22768ce0fabadd64b300dd55e4ba10ce3
Miri <paletaleblue@outlook.com>
2025-10-01 15:54:22 -0600

    114. Centralizar las peticiones - Refactorización. Se cambian los metodos de news.service.ts sin explicacion, se mantienen comentados

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { NewsService } from '../../services/news.service';
+import { Article } from '../../interfaces';
 
 @Component({
   selector: 'app-tab2',
@@ -6,17 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss'],
   standalone: false,
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
   public categories: string[] = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
   public selectedCategory: string = this.categories[0];
+  public articles: Article[] = [];
 
 
-  constructor() {}
+  constructor( private newsService: NewsService) {}
+
+  ngOnInit() {
+    this.newsService.getTopHeadlinesByCategory(this.selectedCategory)
+    .subscribe( articles => {
+      this.articles = [ ...this.articles, ...articles]
+    })
+
+  }
 
   segmentChanged (event: any) {
     this.selectedCategory = event.detail.value;
-    console.log(event.detail.value);
+    this.newsService.getTopHeadlinesByCategory(this.selectedCategory)
+    .subscribe( articles => {
+      this.articles = [ ...articles]
+    })
   }
 
 }
